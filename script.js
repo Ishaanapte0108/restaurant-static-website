83c0510c9fa331fd2546fdd72bce0e4ae8c21582
Ishaanapte0108 <ishaanmapte@gmail.com>
2023-02-03 11:52:22 +0530

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+function fun1() {
+  let x = document.getElementsByClassName("ib1");
+  x.style.background = "#bf6e22";
+}
+function fun2() {
+  document.getElementsByClassName("ib1").style.background = "#fff";
+}
